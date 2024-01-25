76725a5eca8bb93b6ad5f5c130a9f27480af3fd9
Suryakanta Nayak <115612212+suryakantanayakjitu@users.noreply.github.com>
2024-01-25 18:02:59 +0530

    25 Jan Assignment

---
+++ React Day 4 jan 25 Assignment/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FetchEmpData from './Assignment 1 jan 25/FetchEmployeeData';
+// import AjaxDemo1 from './Read data Feom jSON server/axiosData';
+function App() {
+  return (
+    <>
+      <h3 align="center">Welcome to React Applications</h3>
+      <hr />
+      {/* <AjaxDemo1/> */}
+      <FetchEmpData/>
+      {/* <hr/> */}
+    </>
+  );
+}
+
+export default App;
